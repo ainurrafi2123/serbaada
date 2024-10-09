8f5beca7f5ed7c528441373e65eec5711cc7ef19
ainurrafi2123 <ainurrafi2123@gmail.com>
2024-10-09 13:54:52 +0700

    optional

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,5 +1,5 @@
 <?php
-$conn=mysqli_connect('localhost','root','','toko_online');
+$conn=mysqli_connect('localhost','root','','toko_online'); //toko online dapat diubah sesuai nama database anda
 /* check connection */
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
